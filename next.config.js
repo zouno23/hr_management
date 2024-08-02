ff725e145def39c3e615e576f9d1ef401b82123b
zouno23 <medazizbenhmidene@gmail.com>
2024-08-02 13:32:48 +0100

    "Updated next.config.js to import env from src/env.js and replaced process.env with env"

--- next.config.js
+++ next.config.js
@@ -2,18 +2,18 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.js");
+import { env } from "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
     env: {
-      DATABASE_URL: process.env.DATABASE_URL,
-      DIRECT_URL: process.env.DIRECT_URL,
-      NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-      CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
-      NEXT_PUBLIC_CLERK_SIGN_IN_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL,
-      NEXT_PUBLIC_CLERK_SIGN_UP_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL,
-      NODE_ENV: process.env.NODE_ENV,
+      DATABASE_URL: env.DATABASE_URL,
+      DIRECT_URL: env.DIRECT_URL,
+      NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: env.PUBLIC_CLERK_PUBLISHABLE_KEY,
+      CLERK_SECRET_KEY: env.CLERK_SECRET_KEY,
+      NEXT_PUBLIC_CLERK_SIGN_IN_URL: env.PUBLIC_CLERK_SIGN_IN_URL,
+      NEXT_PUBLIC_CLERK_SIGN_UP_URL: env.PUBLIC_CLERK_SIGN_UP_URL,
+      NODE_ENV: env.NODE_ENV,
     },
   };
 
